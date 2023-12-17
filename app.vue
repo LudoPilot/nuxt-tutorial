1c4f17ac9321b699a331615662e821c5fbfc2dab
LudoPilot <e.berardfr@gmail.com>
2023-12-17 13:45:25 +0100

    First commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+	<h1>Bienvenue sur la page d'accueil</h1>
+	<NuxtPage />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true }
+})
+// export default defineAppConfig({
+// 	title: 'Hello Nuxt',
+// 	theme: {
+// 	  dark: true,
+// 	  colors: {
+// 		primary: '#ff0000'
+// 	  }
+// 	}
+//   })
